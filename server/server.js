43edd6ecf5a0fece9eb154d245aea459a516389a
Srlle <srdan.kovacevic@gmail.com>
2017-08-15 17:01:39 +0200

    Add GET/todos:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
-var {mognoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+const {mognoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
 var app = express();
 
@@ -28,6 +29,25 @@ app.get('/todos', (req, res) => {
     res.status(400).send(err);
   });
 });
+
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+
+  }, (error) => {
+    res.status(400).send();
+  })
+});
+
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
