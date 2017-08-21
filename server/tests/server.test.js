a4250cc87c28c3f232fc1e6dbe6df36bfc0ae998
Srlle <srdan.kovacevic@gmail.com>
2017-08-21 16:30:47 +0200

    Add development/test/production environments.

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -165,7 +165,7 @@ describe('PATCH /todos/:id', () => {
         expect(res.body.todo.completed).toBe(true);
         expect(res.body.todo.completedAt).toBeA('number')
       })
-      .end(done)
+      .end(done);
   });
 
   it('should clear completedAt when todo is not completed' ,(done) => {
@@ -182,7 +182,7 @@ describe('PATCH /todos/:id', () => {
         expect(res.body.todo.completed).toBe(false);
         expect(res.body.todo.completedAt).toNotExist()
       })
-      .end(done)
+      .end(done);
   });
 
 });
