599c29f06ed445af1de8c0f9303d464db82dd4e0
Srlle <srdan.kovacevic@gmail.com>
2017-11-11 00:37:39 +0100

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -97,6 +97,27 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  // var user = new User({        //long version
+  //   email: body.email,
+  //   password: body.password
+  // });
+
+  var user = new User(body);    // Short version
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(err);
+  });
+
+});
+
 app.listen(port, () => {
   console.log(`Started on port: ${port}`);
 });
